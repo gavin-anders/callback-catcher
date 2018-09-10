fa399e10c71845d33b9b43004d4a4733aff318f4
Gavin <gavin.anders@googlemail.com>
2018-09-10 14:01:38 +0100

    Added DNS exfiltration support

--- src/catcher/handlers/dns.py
+++ src/catcher/handlers/dns.py
@@ -9,6 +9,7 @@ from catcher.settings import LISTEN_IP
 
 import binascii
 import logging
+import re
 
 logger = logging.getLogger(__name__)
 
@@ -56,9 +57,18 @@ class dns(UdpHandler):
                     logger.info("Dynamic resolving {} to {}".format(qname, resolve))
                     return resolve
                 except:
-                    raise
                     logger.info("Dynamic resolving {} - FAILED".format(qname))
                     return "127.0.0.1"
+            if "exfil" in qname:
+                try:
+                    hexdata = qname.split('.')[1]
+                    if re.match('[0-9a-fA-F]{2}', hexdata):   
+                        logger.info("Extracting data from DNS entry")
+                        raw = binascii.a2b_hex(hexdata)
+                        self.add_secret("DNS Data Exfiltration", raw.decode("utf-8"))
+                except:
+                    logger.info("Extracting data from {} - FAILED".format(qname))
+                return "127.0.0.1"
         elif self.RESOLVED_IP:
             return self.RESOLVED_IP
         return self.server.socket.getsockname()[0]
