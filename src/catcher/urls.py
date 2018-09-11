4c62fab0d5ae49c5809368389becc6d2127e361f
Gavin <gavin.anders@googlemail.com>
2018-09-11 17:13:58 +0100

    Icon
    Added the all important favico.ico

--- src/catcher/urls.py
+++ src/catcher/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 from .views import index, script
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('restapi.urls')),
     url(r'^script.js', script, name="script"),
+    url(r'^favicon\.ico$', favicon_view),
     url(r'^', index, name="index"),
 ]
