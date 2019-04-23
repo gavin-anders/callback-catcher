4af466f816575cdfde2d96b5f6a48060ad74a940
Gavin <gavin.anders@googlemail.com>
2019-04-23 13:15:44 +0100

    Clean Up
    Cleaned up files no longer used

--- src/catcher/management/adduser.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.base import BaseCommand
-
-class Command(BaseCommand):
-    def handle_noargs(self, **options):
-        # now do the things that you want with your models here
-        print("[+] Add a new user")
